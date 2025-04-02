7f69ee9ba4338cc6331b0c83456b6493208a266c
mfreyder <mfreyder@unistra.fr>
2025-04-02 11:37:04 +0200

    Initial commit

---
+++ epl/settings/unittest.py
@@ -0,0 +1,47 @@
+from os import environ
+from pathlib import Path
+
+from .base import *
+
+#######################
+# Debug configuration #
+#######################
+
+DEBUG = True
+
+
+##########################
+# Database configuration #
+##########################
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DEFAULT_DB_TEST_NAME', 'epl'),
+        'USER': environ.get('DEFAULT_DB_TEST_USER', 'epl'),
+        'PASSWORD': environ.get('DEFAULT_DB_TEST_PASSWORD', 'epl'),
+        'HOST': environ.get('DEFAULT_DB_TEST_HOST', 'postgres'),
+        'PORT': environ.get('DEFAULT_DB_TEST_PORT', ''),
+    }
+}
+
+############################
+# Allowed hosts & Security #
+############################
+
+ALLOWED_HOSTS = ['*']
+
+#####################
+# Log configuration #
+#####################
+
+LOGGING['handlers']['file']['filename'] = environ.get(
+    'LOG_DIR',
+    Path('/tmp').resolve(strict=True) / f'test_{SITE_NAME}.log',
+)
+LOGGING['handlers']['file']['level'] = 'DEBUG'
+
+for logger in LOGGING['loggers']:
+    LOGGING['loggers'][logger]['level'] = 'DEBUG'
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
---
+++ run_tests.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "epl.settings.unittest"
+django.setup()
+
+
+"""
+Run tests script
+"""
+
+TestRunner = get_runner(settings)
+test_runner = TestRunner(pattern="test_*.py", verbosity=2, interactive=True, failfast=False)
+
+test_apps = list(settings.LOCAL_APPS)
+test_apps = test_apps if len(sys.argv) <= 1 else sys.argv[1:]
+failures = test_runner.run_tests(test_apps)
+sys.exit(failures)
