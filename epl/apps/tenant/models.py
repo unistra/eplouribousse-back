dafea7dcc7ea458db89bf488345ec01ff801a6a7
dbessey <daniel.bessey@unistra.fr>
2025-04-14 14:13:17 +0200

    🗃️ Store the frontend domain in the domain model, add tenant settings

--- epl/apps/tenant/models.py
+++ epl/apps/tenant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django_tenants.models import DomainMixin, TenantMixin
 
 from epl.models import UUIDPrimaryKeyField
@@ -6,11 +7,13 @@ from epl.models import UUIDPrimaryKeyField
 
 class Consortium(TenantMixin):
     id = UUIDPrimaryKeyField()
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(_("Tenant name"), max_length=100)
+    settings = models.JSONField(_("Tenant settings"), default=dict)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
 
     auto_create_schema = True
 
 
 class Domain(DomainMixin):
     id = UUIDPrimaryKeyField()
+    front_domain = models.CharField(_("Frontend domain"), max_length=253, blank=True)
--- epl/settings/base.py
+++ epl/settings/base.py
@@ -198,7 +198,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_cas.middleware.CASMiddleware",
 ]
 
 # Tenant configuration
