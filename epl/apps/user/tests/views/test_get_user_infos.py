6725c3273b089ddbcaa411487ef8328cfb55b707
mfreyder <mfreyder@unistra.fr>
2025-04-30 14:10:20 +0200

    🧪 Test that unauthenticated and inactive users can't access user infos

--- epl/apps/user/tests/views/test_get_user_infos.py
+++ epl/apps/user/tests/views/test_get_user_infos.py
@@ -26,3 +26,25 @@ class TestUserInfosView(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["username"], self.user.username)
+
+    def test_get_user_infos_unauthenticated(self):
+        """
+        Test that unauthenticated users cannot access user information.
+        """
+        url = reverse("user")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_user_infos_inactive_user(self):
+        """
+        Test that inactive users cannot access user information.
+        """
+
+        with tenant_context(self.tenant):
+            self.user.is_active = False
+            self.user.save()
+
+        url = reverse("user")
+        response = self.client.get(url, user=self.user)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
