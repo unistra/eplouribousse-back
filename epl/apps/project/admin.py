034cebc777a9311531c7364870f8b714998c23da
mfreyder <mfreyder@unistra.fr>
2025-05-09 11:50:09 +0200

    ✨ Declare project and user app to admin.py
    To easily create test data in the tables

--- epl/apps/project/admin.py
+++ epl/apps/project/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from epl.apps.project.models import Project  # Ajustez selon votre structure
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")  # Ajustez selon vos champs
+    search_fields = ("name",)
--- epl/apps/user/admin.py
+++ epl/apps/user/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-from epl.apps.user.models import User
+from epl.apps.user.models import TenantUserRole, User
 
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     pass
+
+
+@admin.register(TenantUserRole)
+class TenantUserRoleAdmin(admin.ModelAdmin):
+    list_display = ("user", "project", "role", "assigned_at", "assigned_by")
+    list_filter = ("role", "project")
+    search_fields = ("user__email", "user__username", "project__name")
+    date_hierarchy = "assigned_at"
