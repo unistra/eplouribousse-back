a60676f532efa70ec2ac700ddee7dcb5c645745e
mfreyder <mfreyder@unistra.fr>
2025-05-21 10:34:04 +0200

    ♻️ Update project API routes to improve structure

--- epl/apps/project/urls.py
+++ epl/apps/project/urls.py
@@ -3,5 +3,5 @@ from rest_framework.routers import DefaultRouter
 from epl.apps.project.views import ProjectViewSet
 
 router = DefaultRouter()
-router.register(r"", ProjectViewSet, basename="project")
+router.register(r"projects", ProjectViewSet, basename="project")
 urlpatterns = router.urls
