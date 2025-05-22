261cc9d9343af7218bf2186ee9ea01eaea056312
mfreyder <mfreyder@unistra.fr>
2025-05-22 16:06:21 +0200

    ✨ Add basic unit tests for Library model, viewset, and serializer

---
+++ epl/apps/project/tests/models/test_library.py
@@ -0,0 +1,47 @@
+from django.db import IntegrityError
+from django_tenants.test.cases import TenantTestCase
+
+from epl.apps.project.models.library import Library
+
+
+class LibraryTest(TenantTestCase):
+    def setUp(self):
+        # Create a library instance for testing
+        self.library = Library.objects.create(
+            name="Bibliothèque Nationale de Test",
+            alias="BNT",
+            code="67000",
+        )
+
+    def test_library_creation(self):
+        self.assertEqual(self.library.name, "Bibliothèque Nationale de Test")
+        self.assertEqual(self.library.alias, "BNT")
+        self.assertEqual(self.library.code, "67000")
+
+    def test_library_str_representation(self):
+        self.assertEqual(str(self.library), "Bibliothèque Nationale de Test")
+
+    def test_library_unique_name_constraint(self):
+        """Test that creating a library with the same name raises an IntegrityError"""
+        with self.assertRaises(IntegrityError):
+            Library.objects.create(
+                name=self.library.name,
+                alias="BNT1",
+                code="67001",
+            )
+
+    def test_library_unique_alias_constraint(self):
+        with self.assertRaises(IntegrityError):
+            Library.objects.create(
+                name="Bibliothèque Nationale de Test 2",
+                alias=self.library.alias,
+                code="67002",
+            )
+
+    def test_library_unique_code_constraint(self):
+        with self.assertRaises(IntegrityError):
+            Library.objects.create(
+                name="Bibliothèque Nationale de Test 3",
+                alias="BNT3",
+                code=self.library.code,
+            )
---
+++ epl/apps/project/tests/views/test_librairy.py
@@ -0,0 +1,31 @@
+from django_tenants.urlresolvers import reverse
+from django_tenants.utils import tenant_context
+
+from epl.apps.project.models.library import Library
+from epl.apps.user.models import User
+from epl.tests import TestCase
+
+
+class LibraryViewsTest(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        with tenant_context(self.tenant):
+            # Create a user
+            self.user = User.objects.create_user(username="user", email="user@eplouribouse.fr")
+            self.library = Library.objects.create(
+                name="Bibliothèque Nationale de Test",
+                alias="BNT",
+                code="67000",
+            )
+
+    def test_get_library_list(self):
+        url = reverse("library-list")
+
+        response = self.get(url, user=self.user)
+        self.assertEqual(response.status_code, 200)
+        print(response.data)
+        print(response.data.get("results"))
+
+        self.assertEqual(len(response.data.get("results")), 1)
+        self.assertEqual(response.data["results"][0]["name"], self.library.name)
