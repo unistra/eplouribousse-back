0877f86b0a514764faa9cc3ced7b07d6cedbd0dd
dbessey <daniel.bessey@unistra.fr>
2025-05-27 17:13:22 +0200

    ✨ Action to assign project creator role to a user

--- epl/apps/user/tests/views/test_user_list.py
+++ epl/apps/user/tests/views/test_user_list.py
@@ -15,27 +15,27 @@ class UserListTest(TestCase):
             self.user = User.objects.create_user(email="user@eplouribousse.fr")
 
     def test_user_must_be_authenticated(self):
-        response = self.get(reverse("list_users"))
+        response = self.get(reverse("user-list"))
         self.response_unauthorized(response)
 
     def test_list_users(self):
-        response = self.get(reverse("list_users"), user=self.user)
+        response = self.get(reverse("user-list"), user=self.user)
         self.response_ok(response)
         self.assertEqual(response.data["count"], 11)
 
     def test_inactive_user_is_not_returned(self):
-        response = self.get(reverse("list_users"), user=self.user, data={"page_size": 20})
+        response = self.get(reverse("user-list"), user=self.user, data={"page_size": 20})
         self.response_ok(response)
         self.assertEqual(response.data["count"], 11)
         self.assertNotIn("inactive@eplouribousse.fr", [user["email"] for user in response.data["results"]])
 
     def test_search_user(self):
-        response = self.get(reverse("list_users"), user=self.user, data={"search": "user@eplouribousse.fr"})
+        response = self.get(reverse("user-list"), user=self.user, data={"search": "user@eplouribousse.fr"})
         self.response_ok(response)
         self.assertEqual(response.data["count"], 1)
         self.assertEqual(response.data["results"][0]["email"], "user@eplouribousse.fr")
 
     def test_search_does_not_return_inactive_user(self):
-        response = self.get(reverse("list_users"), user=self.user, data={"search": "inactive@eplouribousse.fr"})
+        response = self.get(reverse("user-list"), user=self.user, data={"search": "inactive@eplouribousse.fr"})
         self.response_ok(response)
         self.assertEqual(response.data["count"], 0)
--- epl/apps/user/urls.py
+++ epl/apps/user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from epl.apps.user import views
 
-urlpatterns = [
-    path("", views.UserViewSet.as_view({"get": "list"}), name="list_users"),
+router = DefaultRouter()
+router.register(r"", views.UserViewSet, basename="user")
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    # path("", views.UserViewSet.as_view({"get": "list"}), name="list_users"),
     path("change-password/", views.change_password, name="change_password"),
     path("reset-password/", views.reset_password, name="reset_password"),
     path("send-reset-email/", views.send_reset_email, name="send_reset_email"),
