bbce9464676373d65b470761b4d8a3d75df4eb52
dbessey <daniel.bessey@unistra.fr>
2025-06-10 16:38:49 +0200

    🚧 Base of new Collection model

--- epl/validators.py
+++ epl/validators.py
@@ -3,9 +3,12 @@ from pathlib import Path, PosixPath
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import gettext_lazy as _
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError as SchemaValidationError
 from rest_framework import serializers
+from stdnum import issn
+from stdnum.exceptions import ValidationError as StdValidationError
 
 
 class JSONSchemaValidator:
@@ -27,3 +30,19 @@ class JSONSchemaValidator:
             except SchemaValidationError as e:
                 raise serializers.ValidationError(f"Invalid JSON schema: {str(e)}")
         return value
+
+
+class IssnValidator:
+    """
+    Validate that the given ISSN is valid and format uppercase with - separator
+    """
+
+    def __call__(self, value: str) -> str:
+        issn_to_check = value.replace("-", "").replace(" ", "").upper()
+        if len(issn_to_check) != 8:
+            raise serializers.ValidationError(_("ISSN must be 8 characters long"))
+        try:
+            issn.validate(issn_to_check)
+        except StdValidationError as e:
+            raise serializers.ValidationError(f"Invalid ISSN {str(e)}")
+        return f"{issn_to_check[:4]}-{issn_to_check[4:]}"
