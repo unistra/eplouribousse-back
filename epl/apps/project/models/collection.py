c9b3d0c93c1402dd9cec0393eda1152afd6e78ee
dbessey <daniel.bessey@unistra.fr>
2025-06-12 09:00:58 +0200

    Change Collection fields

--- epl/apps/project/models/collection.py
+++ epl/apps/project/models/collection.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from epl.apps.project.models import Project
 from epl.apps.project.models.library import Library
 from epl.models import UUIDPrimaryKeyField
 
@@ -10,7 +11,7 @@ class Collection(models):
     title = models.CharField(_("Title"), max_length=255, db_index=True)
     code = models.CharField(_("Code (PPN or other)"), max_length=25)  # PPN
     library = models.ForeignKey(Library, on_delete=models.CASCADE)  # RCR
-    extra = models.IntegerField(_("Extra copy"), default=0)  # Serial for extra copies
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     issn = models.CharField(_("ISSN"), max_length=9, blank=True)
     call_number = models.CharField(_("Call number"), blank=True)  # Cote
     hold_statement = models.CharField(_("Hold statement"), blank=True)  # État de la collection
@@ -21,9 +22,6 @@ class Collection(models):
     numbering = models.CharField(_("Numbering"), blank=True)  # Numérotation (todo absent des exemples)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["code", "library", "extra"],
-                name="%(app_label)s_%(class)s_unique_extra",
-            )
-        ]
+        verbose_name = _("Collection")
+        verbose_name_plural = _("Collections")
+        ordering = ["title"]
