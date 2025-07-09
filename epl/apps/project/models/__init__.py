94283989cf0586f5693ab29e58e9500fbf8caccf
dbessey <daniel.bessey@unistra.fr>
2025-07-09 12:09:25 +0200

    ✨ Add ActionLog model
    Package operations: 2 installs, 0 updates, 0 removals
    
      - Installing python-ipware (3.0.0)
      - Installing django-ipware (7.0.1)

--- epl/apps/project/models/__init__.py
+++ epl/apps/project/models/__init__.py
@@ -1,8 +1,10 @@
 from epl.apps.project.models.collection import Collection
 from epl.apps.project.models.library import Library
+from epl.apps.project.models.logging import ActionLog
 from epl.apps.project.models.project import Project, Role, Status, UserRole
 
 __all__ = [
+    "ActionLog",
     "Collection",
     "Library",
     "Project",
---
+++ epl/apps/project/models/logging.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import typing
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from epl.models import UUIDPrimaryKeyField
+
+if typing.TYPE_CHECKING:
+    from epl.apps.user.models import User  # noqa: F401
+
+
+class ActionLog(models.Model):
+    id = UUIDPrimaryKeyField()
+    action_message = models.CharField(max_length=255)
+    action_time = models.DateTimeField(auto_now=True, db_index=True)
+    actor = models.CharField(max_length=255)
+    ip = models.GenericIPAddressField(protocol="both", unpack_ipv4=True)
+
+    content_object = GenericForeignKey("content_type", "object_id")
+    object_id = models.UUIDField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+
+    created_by = models.ForeignKey("user.User", on_delete=models.SET_NULL, related_name="log_entries", null=True)
+
+    class Meta:
+        verbose_name = "Log Entry"
+        verbose_name_plural = "Log Entries"
+        ordering = ["-action_time"]
+
+    def __str__(self):
+        return f"{self.action_time:%Y-%m-%d %H:%M:%S} - {self.action_message} by {self.actor} ({self.ip}) on {self.content_object}"
+
+    @classmethod
+    def log(cls, message: str, actor: User, ip: str = "", obj: models.Model = None):
+        ActionLog.objects.create(
+            action_message=message,
+            actor=f"{actor.first_name} {actor.last_name} <{actor.username}>".strip(),
+            ip=ip,
+            content_object=obj,
+            created_by=actor,
+        )
