87593688f12361c6ea70bc6d63c5aebd6cc219e3
mhmae <mhmae@unsitra.fr>
2025-07-16 13:38:02 +0200

    ✨ Modify ProjectDetailSerializer to add is_alternative_storage_site key to associated libraries

--- epl/apps/project/models/__init__.py
+++ epl/apps/project/models/__init__.py
@@ -1,14 +1,6 @@
 from epl.apps.project.models.collection import Collection
 from epl.apps.project.models.library import Library
 from epl.apps.project.models.logging import ActionLog
-from epl.apps.project.models.project import Project, Role, Status, UserRole
+from epl.apps.project.models.project import Project, ProjectLibrary, Role, Status, UserRole
 
-__all__ = [
-    "ActionLog",
-    "Collection",
-    "Library",
-    "Project",
-    "Role",
-    "Status",
-    "UserRole",
-]
+__all__ = ["ActionLog", "Collection", "Library", "Project", "Role", "Status", "UserRole", "ProjectLibrary"]
