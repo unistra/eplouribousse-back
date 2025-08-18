47f568e5c0c0bb7366f0836f4a572d1777fe214a
mhmae <mhmae@unsitra.fr>
2025-08-18 08:46:46 +0200

    ✨ Add 'should_position' field to ResourceSerializer to indicate if the user can position the resource

--- epl/apps/project/serializers/resource.py
+++ epl/apps/project/serializers/resource.py
@@ -1,7 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
-from epl.apps.project.models import Resource
+from epl.apps.project.models import Collection, Resource
 from epl.apps.project.serializers.collection import CollectionPositioningSerializer
 from epl.apps.user.models import User
 from epl.services.permissions.serializers import AclField, AclSerializerMixin
@@ -14,10 +14,24 @@ class ResourceSerializer(AclSerializerMixin, serializers.ModelSerializer):
     should_instruct = serializers.SerializerMethodField(
         read_only=True, help_text=_("Indicates if the user should instruct this resource")
     )
+    should_position = serializers.SerializerMethodField(
+        read_only=True, help_text=_("Indicates if the user should position this resource")
+    )
 
     class Meta:
         model = Resource
-        fields = ["id", "title", "code", "count", "call_numbers", "should_instruct", "status", "arbitration", "acl"]
+        fields = [
+            "id",
+            "title",
+            "code",
+            "count",
+            "call_numbers",
+            "should_instruct",
+            "should_position",
+            "status",
+            "arbitration",
+            "acl",
+        ]
 
     def get_should_instruct(self, obj: Resource) -> bool:
         library_id = obj.next_turn
@@ -26,6 +40,17 @@ class ResourceSerializer(AclSerializerMixin, serializers.ModelSerializer):
         if not user or not user.is_authenticated or not library_id:
             return False
         return user.is_instructor(obj.project, library_id)
+
+    def get_should_position(self, obj: Resource) -> bool:
+        user: User = self.context.get("request").user
+        library_id_selected = self.context.get("request").query_params.get("library")
+
+        if library_id_selected and user.is_instructor(project=obj.project, library=library_id_selected):
+            return Collection.objects.filter(
+                resource=obj, library_id=library_id_selected, position=None, exclusion_reason__in=["", None]
+            ).exists()
+
+        return False
 
 
 class ResourceWithCollectionsSerializer(serializers.Serializer):
