e4db1415d16104e9ec633fccea9e12ccf889f7fe
mhmae <mhmae@unsitra.fr>
2025-08-27 11:19:14 +0200

    ✨ Add Segment model with related fields and constraints for collection segments

--- epl/apps/project/models/__init__.py
+++ epl/apps/project/models/__init__.py
@@ -3,12 +3,8 @@ from epl.apps.project.models.collection import Collection, Resource
 from epl.apps.project.models.comment import Comment
 from epl.apps.project.models.library import Library
 from epl.apps.project.models.logging import ActionLog
-from epl.apps.project.models.project import (
-    Project,
-    ProjectLibrary,
-    Role,
-    UserRole,
-)
+from epl.apps.project.models.project import Project, ProjectLibrary, Role, UserRole
+from epl.apps.project.models.segment import Segment
 
 __all__ = [
     "ActionLog",
@@ -22,4 +18,5 @@ __all__ = [
     "Role",
     "ProjectStatus",
     "UserRole",
+    "Segment",
 ]
---
+++ epl/apps/project/models/segment.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from epl.apps.project.models.choices import SegmentType
+from epl.models import UUIDPrimaryKeyField
+
+
+class Segment(models.Model):
+    id = UUIDPrimaryKeyField()
+    segment_type = models.CharField(
+        choices=SegmentType.choices,
+        default=SegmentType.BOUND,
+        verbose_name=_("Segment type"),
+    )
+    content = models.TextField(_("Segment"), blank=False)
+    improvable_elements = models.TextField(_("Improbable elements"), blank=True)
+    exception = models.TextField(_("Exception"), blank=True)
+
+    improved_segment = models.ForeignKey(
+        "project.Segment", on_delete=models.CASCADE, related_name="improving_segments", null=True
+    )
+
+    collection = models.ForeignKey(
+        "project.Collection", on_delete=models.CASCADE, related_name="segments", null=False, blank=False
+    )
+    order = models.PositiveSmallIntegerField(null=False, blank=False)
+    retained = models.BooleanField(default=False)
+    created_by = models.ForeignKey("user.User", on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Collection segment")
+        verbose_name_plural = _("Collection segments")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["collection", "order"],
+                name="unique_collection_order",
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.collection.project} - {_('Segment')} n°{self.order}: {self.content}"
