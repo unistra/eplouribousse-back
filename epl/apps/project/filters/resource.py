e177de9ad98a869da4f3a3fe924e6412490107ca
dbessey <daniel.bessey@unistra.fr>
2025-09-05 14:46:18 +0200

    Filter resources by status

--- epl/apps/project/filters/resource.py
+++ epl/apps/project/filters/resource.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import filters
 from rest_framework.exceptions import ValidationError
 
-from epl.apps.project.models import Library, Project
+from epl.apps.project.models import Library, Project, ResourceStatus
 
 
 class ResourceFilter(filters.BaseFilterBackend):
@@ -14,6 +14,8 @@ class ResourceFilter(filters.BaseFilterBackend):
     library_param_description = _("Library ID to which the collection belongs")
     against_param = "against"
     against_param_description = _("ID of the library to compare against")
+    status_param = "status"
+    status_param_description = _("Filter by resource status")
 
     def filter_queryset(self, request, queryset, view):
         if project_id := request.query_params.get(self.project_param, None):
@@ -29,6 +31,22 @@ class ResourceFilter(filters.BaseFilterBackend):
             except (Library.DoesNotExist, DjangoValidationError):
                 raise ValidationError({"library": _("Library not found")})
 
+        if status := int(request.query_params.get(self.status_param, 0)):
+            if status == ResourceStatus.POSITIONING:
+                # If a Resource is in Instruction or Control positioning status but does
+                # not have any segments assigned yet, we consider it can still be positioned.
+                queryset = queryset.filter(
+                    Q(status=ResourceStatus.POSITIONING) | Q(collections__segments__isnull=True)
+                ).distinct()
+            elif status in [
+                ResourceStatus.INSTRUCTION_BOUND,
+                ResourceStatus.CONTROL_BOUND,
+                ResourceStatus.INSTRUCTION_UNBOUND,
+                ResourceStatus.CONTROL_UNBOUND,
+            ]:
+                queryset = queryset.filter(status=status)
+            else:
+                raise ValidationError({"status": _("Invalid status value")})
         if against_id := request.query_params.get(self.against_param, None):
             try:
                 _against_library = Library.objects.filter(id=against_id).exists()
@@ -84,6 +102,22 @@ class ResourceFilter(filters.BaseFilterBackend):
                     "type": "string",
                     "format": "uuid",
                     "pattern": "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
+                },
+            },
+            {
+                "name": self.status_param,
+                "required": False,
+                "in": "query",
+                "description": str(self.status_param_description),
+                "schema": {
+                    "type": "integer",
+                    "enum": [
+                        ResourceStatus.POSITIONING,
+                        ResourceStatus.INSTRUCTION_BOUND,
+                        ResourceStatus.CONTROL_BOUND,
+                        ResourceStatus.INSTRUCTION_UNBOUND,
+                        ResourceStatus.CONTROL_UNBOUND,
+                    ],
                 },
             },
         ]
