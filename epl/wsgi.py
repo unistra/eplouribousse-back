847da403614ba40c3b8936b4b410d68f4d944506
dbessey <daniel.bessey@unistra.fr>
2026-01-21 14:47:45 +0100

    🗑️ Remove "{{ goal }}" from wsgi.py and manage.py

--- epl/wsgi.py
+++ epl/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epl.settings.{{ goal }}")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epl.settings.docker")
 
 application = get_wsgi_application()
